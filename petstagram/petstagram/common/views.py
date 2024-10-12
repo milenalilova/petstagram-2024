b310759095b6c85292ca228dc7197d02dab1f682
milenalilova <milenalilova78@gmail.com>
2024-10-12 18:50:43 -0300

    Modified view and home page template

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from petstagram.photos.models import Photo
+
 
 def index(request):
-    return render(request, 'common/home-page.html')
+    all_photos = Photo.objects.all()
+
+    context = {'all_photos': all_photos}
+
+    return render(request, 'common/home-page.html', context)
